c7805969d1e124b06eee071e1c8e7c9ed4a7ec29
Alexey Kuleshevich <alexey@kuleshevi.ch>
2019-09-15 02:10:53 +0300

    WIP: generalizing stuff

--- color-space/src/Graphics/ColorSpace/Internal.hs
+++ color-space/src/Graphics/ColorSpace/Internal.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 -- |
 -- Module      : Graphics.ColorSpace.Internal
@@ -15,6 +16,24 @@ module Graphics.ColorSpace.Internal
   , ColorSpace(..)
   , AlphaSpace(..)
   , module Graphics.ColorSpace.Elevator
+  , showsP
+  , shows3
+  , shows4
+  , shows5
+  , foldr3
+  , foldr4
+  , foldr5
+  , traverse3
+  , traverse4
+  , traverse5
+  , sizeOfN
+  , alignmentN
+  , peek3
+  , poke3
+  , peek4
+  , poke4
+  , peek5
+  , poke5
   ) where
 
 import Control.Applicative
@@ -27,32 +46,36 @@ import qualified Data.Vector.Generic as V
 import qualified Data.Vector.Generic.Mutable as VM
 import qualified Data.Vector.Storable as VS
 import qualified Data.Vector.Unboxed as VU
+import Foreign.Ptr
+import Foreign.Storable
 import Graphics.ColorSpace.Elevator
 
 
 -- | A Pixel family with a color space and a precision of elements.
 data family Pixel cs e :: *
 
-class (Eq cs, Enum cs, Show cs, Bounded cs, Typeable cs,
-       Functor (Pixel cs), Applicative (Pixel cs), Foldable (Pixel cs), Traversable (Pixel cs),
-       Eq (Pixel cs e), VU.Unbox (Components cs e), VS.Storable (Pixel cs e), Elevator e)
-      => ColorSpace cs e where
-
+class ( Functor (Pixel cs)
+      , Applicative (Pixel cs)
+      , Foldable (Pixel cs)
+      , Traversable (Pixel cs)
+      , Eq (Pixel cs e)
+      , VU.Unbox (Components cs e)
+      , VS.Storable (Pixel cs e)
+      , Typeable cs
+      , Elevator e
+      ) =>
+      ColorSpace cs e
+  where
   type Components cs e
-
   -- | Convert a Pixel to a representation suitable for storage as an unboxed
   -- element, usually a tuple of channels.
   toComponents :: Pixel cs e -> Components cs e
-
   -- | Convert from an elemnt representation back to a Pixel.
   fromComponents :: Components cs e -> Pixel cs e
-
   -- | Retrieve Pixel's component value
   getPxC :: Pixel cs e -> cs -> e
-
   -- | Set Pixel's component value
   setPxC :: Pixel cs e -> cs -> e -> Pixel cs e
-
   -- | Map a channel aware function over all Pixel's components.
   mapPxC :: (cs -> e -> e) -> Pixel cs e -> Pixel cs e
 
@@ -200,3 +223,108 @@ instance (ColorSpace cs e) => V.Vector VU.Vector (Pixel cs e) where
   {-# INLINE basicUnsafeCopy #-}
   elemseq (V_Pixel vec) val = V.elemseq vec (toComponents val)
   {-# INLINE elemseq #-}
+
+
+-- Show helpers
+
+channelSeparator :: ShowS
+channelSeparator = ('|':)
+
+shows3 :: Show a => a -> a -> a -> ShowS
+shows3 c0 c1 c2 = shows c0 . channelSeparator . shows c1 . channelSeparator . shows c2
+
+shows4 :: Show a => a -> a -> a -> a -> ShowS
+shows4 c0 c1 c2 c3 = shows c0 . channelSeparator . shows3 c1 c2 c3
+
+shows5 :: Show a => a -> a -> a -> a -> a -> ShowS
+shows5 c0 c1 c2 c3 c4 = shows c0 . channelSeparator . shows4 c1 c2 c3 c4
+
+showsP :: String -> ShowS -> ShowS
+showsP t i = ('<' :) . (t ++) . (":(" ++) . i . (")>" ++)
+
+-- Foldable helpers
+
+foldr3 :: (e -> a -> a) -> a -> e -> e -> e -> a
+foldr3 f acc c0 c1 c2 = f c0 (f c1 (f c2 acc))
+{-# INLINE foldr3 #-}
+
+foldr4 :: (e -> a -> a) -> a -> e -> e -> e -> e -> a
+foldr4 f acc c0 c1 c2 c3 = f c0 (f c1 (f c2 (f c3 acc)))
+{-# INLINE foldr4 #-}
+
+foldr5 :: (e -> a -> a) -> a -> e -> e -> e -> e -> e -> a
+foldr5 f acc c0 c1 c2 c3 c4 = f c0 (f c1 (f c2 (f c3 (f c4 acc))))
+{-# INLINE foldr5 #-}
+
+traverse3 :: Applicative f => (a -> a -> a -> b) -> (t -> f a) -> t -> t -> t -> f b
+traverse3 g f c0 c1 c2 = g <$> f c0 <*> f c1 <*> f c2
+{-# INLINE traverse3 #-}
+
+traverse4 :: Applicative f => (a -> a -> a -> a -> b) -> (t -> f a) -> t -> t -> t -> t -> f b
+traverse4 g f c0 c1 c2 c3 = g <$> f c0 <*> f c1 <*> f c2 <*> f c3
+{-# INLINE traverse4 #-}
+
+traverse5 ::
+     Applicative f => (a -> a -> a -> a -> a -> b) -> (t -> f a) -> t -> t -> t -> t -> t -> f b
+traverse5 g f c0 c1 c2 c3 c4 = g <$> f c0 <*> f c1 <*> f c2 <*> f c3 <*> f c4
+{-# INLINE traverse5 #-}
+
+
+-- Storable helpers
+
+sizeOfN :: forall cs e . Storable e => Int -> Pixel cs e -> Int
+sizeOfN n _ = n * sizeOf (undefined :: e)
+{-# INLINE sizeOfN #-}
+
+alignmentN :: forall cs e . Storable e => Int -> Pixel cs e -> Int
+alignmentN _ _ = alignment (undefined :: e)
+{-# INLINE alignmentN #-}
+
+peek3 :: Storable e => (e -> e -> e -> Pixel cs e) -> Ptr (Pixel cs e) -> IO (Pixel cs e)
+peek3 f p = do
+  let q = castPtr p
+  c0 <- peek q
+  c1 <- peekElemOff q 1
+  c2 <- peekElemOff q 2
+  return $! f c0 c1 c2
+{-# INLINE peek3 #-}
+
+poke3 :: Storable e => Ptr (Pixel cs e) -> e -> e -> e -> IO ()
+poke3 p c0 c1 c2 = do
+  let q = castPtr p
+  poke q c0
+  pokeElemOff q 1 c1
+  pokeElemOff q 2 c2
+{-# INLINE poke3 #-}
+
+peek4 ::
+     forall cs e. Storable e
+  => (e -> e -> e -> e -> Pixel cs e)
+  -> Ptr (Pixel cs e)
+  -> IO (Pixel cs e)
+peek4 f p = do
+  c0 <- peek (castPtr p)
+  peek3 (f c0) (p `plusPtr` sizeOf (undefined :: e))
+{-# INLINE peek4 #-}
+
+poke4 :: forall cs e . Storable e => Ptr (Pixel cs e) -> e -> e -> e -> e -> IO ()
+poke4 p c0 c1 c2 c3 = do
+  poke (castPtr p) c0
+  poke3 (p `plusPtr` sizeOf (undefined :: e)) c1 c2 c3
+{-# INLINE poke4 #-}
+
+peek5 ::
+     forall cs e. Storable e
+  => (e -> e -> e -> e -> e -> Pixel cs e)
+  -> Ptr (Pixel cs e)
+  -> IO (Pixel cs e)
+peek5 f p = do
+  c0 <- peek (castPtr p)
+  peek4 (f c0) (p `plusPtr` sizeOf (undefined :: e))
+{-# INLINE peek5 #-}
+
+poke5 :: forall cs e . Storable e => Ptr (Pixel cs e) -> e -> e -> e -> e -> e -> IO ()
+poke5 p c0 c1 c2 c3 c4 = do
+  poke (castPtr p) c0
+  poke4 (p `plusPtr` sizeOf (undefined :: e)) c1 c2 c3 c4
+{-# INLINE poke5 #-}
--- color-space/src/Graphics/ColorSpace/RGB.hs
+++ color-space/src/Graphics/ColorSpace/RGB.hs
@@ -27,41 +27,24 @@ import Prelude hiding (map)
 --- RGB ---
 -----------
 
--- | Red, Green and Blue color space.
-data RGB = RedRGB
-         | GreenRGB
-         | BlueRGB deriving (Eq, Enum, Show, Bounded, Typeable)
+-- | The most common @sRGB@ color space
+data RGB
 
-
-data instance Pixel RGB e = PixelRGB !e !e !e deriving (Eq, Ord)
+data instance Pixel RGB e = PixelRGB !e !e !e deriving (Eq, Ord, Bounded)
 
 instance Show e => Show (Pixel RGB e) where
-  show (PixelRGB r g b) = "<RGB:("++show r++"|"++show g++"|"++show b++")>"
-
+  showsPrec _ (PixelRGB r g b) = showsP "sRGB" (shows3 r g b)
 
 instance Elevator e => ColorSpace RGB e where
   type Components RGB e = (e, e, e)
-
   toComponents (PixelRGB r g b) = (r, g, b)
   {-# INLINE toComponents #-}
   fromComponents (r, g, b) = PixelRGB r g b
   {-# INLINE fromComponents #-}
-  getPxC (PixelRGB r _ _) RedRGB   = r
-  getPxC (PixelRGB _ g _) GreenRGB = g
-  getPxC (PixelRGB _ _ b) BlueRGB  = b
-  {-# INLINE getPxC #-}
-  setPxC (PixelRGB _ g b) RedRGB   r = PixelRGB r g b
-  setPxC (PixelRGB r _ b) GreenRGB g = PixelRGB r g b
-  setPxC (PixelRGB r g _) BlueRGB  b = PixelRGB r g b
-  {-# INLINE setPxC #-}
-  mapPxC f (PixelRGB r g b) = PixelRGB (f RedRGB r) (f GreenRGB g) (f BlueRGB b)
-  {-# INLINE mapPxC #-}
-
 
 instance Functor (Pixel RGB) where
   fmap f (PixelRGB r g b) = PixelRGB (f r) (f g) (f b)
   {-# INLINE fmap #-}
-
 
 instance Applicative (Pixel RGB) where
   pure !e = PixelRGB e e e
@@ -69,52 +52,38 @@ instance Applicative (Pixel RGB) where
   (PixelRGB fr fg fb) <*> (PixelRGB r g b) = PixelRGB (fr r) (fg g) (fb b)
   {-# INLINE (<*>) #-}
 
-
 instance Foldable (Pixel RGB) where
-  foldr f !z (PixelRGB r g b) = f r (f g (f b z))
+  foldr f !z (PixelRGB r g b) = foldr3 f z r g b
   {-# INLINE foldr #-}
 
-
 instance Traversable (Pixel RGB) where
-  traverse f (PixelRGB r g b) = PixelRGB <$> f r <*> f g <*> f b
+  traverse f (PixelRGB r g b) = traverse3 PixelRGB f r g b
   {-# INLINE traverse #-}
 
 instance Storable e => Storable (Pixel RGB e) where
-  sizeOf _ = 3 * sizeOf (undefined :: e)
+  sizeOf = sizeOfN 3
   {-# INLINE sizeOf #-}
-  alignment _ = alignment (undefined :: e)
+  alignment = alignmentN 3
   {-# INLINE alignment #-}
-  peek !p = do
-    let !q = castPtr p
-    r <- peek q
-    g <- peekElemOff q 1
-    b <- peekElemOff q 2
-    return $! PixelRGB r g b
+  peek = peek3 PixelRGB
   {-# INLINE peek #-}
-  poke !p (PixelRGB r g b) = do
-    let !q = castPtr p
-    poke q r
-    pokeElemOff q 1 g
-    pokeElemOff q 2 b
+  poke p (PixelRGB r g b) = poke3 p r g b
   {-# INLINE poke #-}
-
 
 ------------
 --- RGBA ---
 ------------
 
 
--- | Red, Green and Blue color space with Alpha channel.
-data RGBA = RedRGBA
-          | GreenRGBA
-          | BlueRGBA
-          | AlphaRGBA deriving (Eq, Enum, Show, Bounded, Typeable)
+-- | @sRGB@ color space with Alpha channel.
+data RGBA
+
 
 data instance Pixel RGBA e = PixelRGBA !e !e !e !e deriving (Eq, Ord)
 
 
 instance Show e => Show (Pixel RGBA e) where
-  show (PixelRGBA r g b a) = "<RGBA:("++show r++"|"++show g++"|"++show b++"|"++show a++")>"
+  showsPrec _ (PixelRGBA r g b a) = showsP "sRGBA" (shows4 r g b a)
 
 
 instance Elevator e => ColorSpace RGBA e where
@@ -124,24 +93,9 @@ instance Elevator e => ColorSpace RGBA e where
   {-# INLINE toComponents #-}
   fromComponents (r, g, b, a) = PixelRGBA r g b a
   {-# INLINE fromComponents #-}
-  getPxC (PixelRGBA r _ _ _) RedRGBA   = r
-  getPxC (PixelRGBA _ g _ _) GreenRGBA = g
-  getPxC (PixelRGBA _ _ b _) BlueRGBA  = b
-  getPxC (PixelRGBA _ _ _ a) AlphaRGBA = a
-  {-# INLINE getPxC #-}
-  setPxC (PixelRGBA _ g b a) RedRGBA   r = PixelRGBA r g b a
-  setPxC (PixelRGBA r _ b a) GreenRGBA g = PixelRGBA r g b a
-  setPxC (PixelRGBA r g _ a) BlueRGBA  b = PixelRGBA r g b a
-  setPxC (PixelRGBA r g b _) AlphaRGBA a = PixelRGBA r g b a
-  {-# INLINE setPxC #-}
-  mapPxC f (PixelRGBA r g b a) =
-    PixelRGBA (f RedRGBA r) (f GreenRGBA g) (f BlueRGBA b) (f AlphaRGBA a)
-  {-# INLINE mapPxC #-}
-
 
 instance Elevator e => AlphaSpace RGBA e where
   type Opaque RGBA = RGB
-
   getAlpha (PixelRGBA _ _ _ a) = a
   {-# INLINE getAlpha #-}
   addAlpha !a (PixelRGB r g b) = PixelRGBA r g b a
@@ -161,30 +115,19 @@ instance Applicative (Pixel RGBA) where
   {-# INLINE (<*>) #-}
 
 instance Foldable (Pixel RGBA) where
-  foldr f !z (PixelRGBA r g b a) = f r (f g (f b (f a z)))
+  foldr f !z (PixelRGBA r g b a) = foldr4 f z r g b a
   {-# INLINE foldr #-}
 
 instance Traversable (Pixel RGBA) where
-  traverse f (PixelRGBA r g b a) = PixelRGBA <$> f r <*> f g <*> f b <*> f a
+  traverse f (PixelRGBA r g b a) = traverse4 PixelRGBA f r g b a
   {-# INLINE traverse #-}
 
 instance Storable e => Storable (Pixel RGBA e) where
-  sizeOf _ = 4 * sizeOf (undefined :: e)
+  sizeOf = sizeOfN 4
   {-# INLINE sizeOf #-}
-  alignment _ = alignment (undefined :: e)
+  alignment = alignmentN 4
   {-# INLINE alignment #-}
-  peek p = do
-    let q = castPtr p
-    r <- peek q
-    g <- peekElemOff q 1
-    b <- peekElemOff q 2
-    a <- peekElemOff q 3
-    return $! PixelRGBA r g b a
+  peek = peek4 PixelRGBA
   {-# INLINE peek #-}
-  poke p (PixelRGBA r g b a) = do
-    let q = castPtr p
-    poke q r
-    pokeElemOff q 1 g
-    pokeElemOff q 2 b
-    pokeElemOff q 3 a
+  poke p (PixelRGBA r g b a) = poke4 p r g b a
   {-# INLINE poke #-}
