e5674ba3d446c69197662a06f5984fe17f92eaef
Alexey Kuleshevich <alexey@kuleshevi.ch>
2019-11-21 04:10:30 +0300

    Add missing Graphics/ColorSpace/RGB/Alternative.hs

---
+++ color-space/src/Graphics/ColorSpace/RGB/Alternative.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE PatternSynonyms #-}
+-- |
+-- Module      : Graphics.ColorSpace.RGB.Alternative
+-- Copyright   : (c) Alexey Kuleshevich 2018-2019
+-- License     : BSD3
+-- Maintainer  : Alexey Kuleshevich <lehins@yandex.ru>
+-- Stability   : experimental
+-- Portability : non-portable
+--
+
+module Graphics.ColorSpace.RGB.Alternative
+  ( module X
+  ) where
+
+import Graphics.ColorSpace.RGB.Alternative.HSI as X
+import Graphics.ColorSpace.RGB.Alternative.HSL as X
+import Graphics.ColorSpace.RGB.Alternative.HSV as X
+import Graphics.ColorSpace.RGB.Alternative.YCbCr as X
