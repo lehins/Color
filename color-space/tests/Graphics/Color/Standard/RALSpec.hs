91bf5aadd644dc04c86cd673aad61231b5678e0f
Alexey Kuleshevich <alexey@kuleshevi.ch>
2019-12-28 21:27:07 +0300

    Started on RAL spec

---
+++ color-space/tests/Graphics/Color/Standard/RALSpec.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+module Graphics.Color.Model.RALSpec (spec) where
+
+import Graphics.Color.Model.Common
+import Graphics.Color.Standard.RAL
+
+spec :: Spec
+spec = pure ()
