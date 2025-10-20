c67ca46fba78b0fbd61183508a10fd12e8d4dde9
Alexey Kuleshevich <alexey@kuleshevi.ch>
2025-10-19 22:21:53 -0600

    fourmolus script

--- scripts/doctest.sh
+++ scripts/doctest.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-cabal repl Color --build-depends=QuickCheck --build-depends=JuicyPixels --build-depends=QuickCheck --with-compiler=doctest --repl-options='-w -Wdefault'
+cabal repl Color --build-depends=QuickCheck --build-depends=JuicyPixels --with-compiler=doctest --repl-options='-w -Wdefault'
